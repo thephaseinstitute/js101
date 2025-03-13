0dd39848efa602281f289d1425f6071d2641c898
Alex Ramsdell <alexramsdell@gmail.com>
2025-03-13 11:23:05 -0400

    js101 video 1: basics

---
+++ index.js
@@ -0,0 +1,15 @@
+let note = "Ab";
+console.log(note);
+
+note = "Bb";
+console.log(note);
+
+let octave = 4;
+
+console.log(octave);
+
+octave += 1; // octave = octave - 1
+console.log(octave);
+
+
+console.log('note: ' + note + octave);

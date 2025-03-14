12958710230b0fd444e6dd4a7a8ac657d4785886
Alex Ramsdell <alexramsdell@gmail.com>
2025-03-14 09:10:03 -0400

    part 2. objects, dot access.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-let note = "Ab";
-console.log(note);
+const note = 'Ab';
+console.log(note.length);
 
-note = "Bb";
-console.log(note);
+const midiMessage = {
+  velocity: 127,
+  noteNumber: 60,
+  type: 'note-on'
+};
 
-let octave = 4;
+console.log(midiMessage.velocity);
+console.log(midiMessage.noteNumber);
+console.log(midiMessage.type);
+console.log(midiMessage.type.length);
 
-console.log(octave);
-
-octave += 1; // octave = octave - 1
-console.log(octave);
+console.log(midiMessage["t" + "ype"]["length"]);
 
 
-console.log('note: ' + note + octave);
